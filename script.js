d2439afcf954aa5b0c67370fa2e49875434df337
Roland Stedy <stedyroland@gmail.com>
2024-05-18 21:01:08 +0300

    Getting Started

---
+++ script.js
@@ -0,0 +1,17 @@
+let one=0;
+let gold=50;
+let heath=100;
+let currentWeapon=0;
+let fighting;
+let monsterHealth;
+let inventory=["stick"]
+
+const btnOne=document.getElementById("btn-1")
+let btnTwo=document.getElementById("btn-2")
+let btnThree=document.getElementById("btn-3")
+let text=document.getElementById("text")
+let oneText=document.getElementById("one")
+let goldText=document.querySelector("#gold")
+let heathText=document.getElementById("health")
+
+onclick=(btnOne.innerText("Haha haha"));
